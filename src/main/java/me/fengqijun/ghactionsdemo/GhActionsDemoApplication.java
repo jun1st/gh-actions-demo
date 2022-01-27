a4e53550589515c4a78a6fa281de1383d9f54a5a
fengd-arm <jun1st.feng@gmail.com>
2022-01-27 16:45:06 +0800

    Initial commit

---
+++ src/main/java/me/fengqijun/ghactionsdemo/GhActionsDemoApplication.java
@@ -0,0 +1,16 @@
+package me.fengqijun.ghactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fengqijun
+ */
+@SpringBootApplication
+public class GhActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhActionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/fengqijun/ghactionsdemo/GhActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fengqijun.ghactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
