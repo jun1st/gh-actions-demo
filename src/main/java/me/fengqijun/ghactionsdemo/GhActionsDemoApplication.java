bb456ab46eb43daec817e2cb860b179d86633ae7
fengd-arm <jun1st.feng@gmail.com>
2022-01-28 14:26:36 +0800

    start hello,

--- src/main/java/me/fengqijun/ghactionsdemo/GhActionsDemoApplication.java
+++ src/main/java/me/fengqijun/ghactionsdemo/GhActionsDemoApplication.java
@@ -2,15 +2,23 @@ package me.fengqijun.ghactionsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author fengqijun
  */
 @SpringBootApplication
+@RestController
 public class GhActionsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GhActionsDemoApplication.class, args);
 	}
 
+
+	@GetMapping("/")
+	public String hello() {
+		return "{\"hello\" : \"world\"}";
+	}
 }
